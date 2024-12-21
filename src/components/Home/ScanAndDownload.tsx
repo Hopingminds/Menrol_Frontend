cc04cd7e62f9cde4ee498b6ca53b11331dce2507
1karan0 <karan523thakur@gmail.com>
2024-12-21 17:58:55 +0530

    mobile version

--- src/components/Home/ScanAndDownload.tsx
+++ src/components/Home/ScanAndDownload.tsx
@@ -3,25 +3,26 @@ import React from "react";
 
 const ScanAndDownload = () => {
   return (
-    <div className="bg-[#0054A5] h-[80vh] w-full flex justify-center relative">
+    <div className="bg-[#0054A5] h-[80vh] xsm:h-[50vh] w-full flex  justify-center  relative xsm:w-full">
       {/* Text Section */}
-      <div>
-        <div className="text-white pt-10">
-          <h1 className="text-[56px] font-bold">Scan to Download App</h1>
-          <p className="text-[20px] mb-4">
+      
+        <div className="text-white pt-10 xsm:p-6">
+          <h1 className="text-[56px] font-bold xsm:text-[30px]">Scan to Download App</h1>
+          <p className="text-[20px] mb-4 xsm:text-[10px]">
             Available on your favorite store. Start your premium experience now
           </p>
         </div>
-      </div>
+      
 
       {/* Image Section */}
-      <div className="absolute bottom-0 w-full flex flex-col items-center gap-6">
+      <div className="absolute bottom-0 w-full flex flex-col items-center gap-6 ">
         <div className="flex flex-row gap-x-6">
-          <div className="flex flex-col gap-3">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col items-center gap-3 top-6 xsm:mt-64 ">
             <Image
               src="/Images/Scanner.png"
               alt="Scanner"
-              className="h-[25vh] w-[10vw] pt-10"
+              className=" w-[80%] pt-10"
               height={200}
               width={200}
             />
@@ -34,11 +35,11 @@ const ScanAndDownload = () => {
             />
           </div>
 
-          <div className="flex flex-col gap-3 top-6">
+          <div className="flex flex-col items-center gap-3 top-6 xsm:mt-64">
             <Image
               src="/Images/Scanner.png"
               alt="Scanner"
-              className="h-[25vh] w-[10vw] pt-10 object-cover"
+              className=" w-[80%] pt-10 object-cover"
               height={200}
               width={200}
             />
@@ -51,14 +52,15 @@ const ScanAndDownload = () => {
               width={200}
             />
           </div>
+          </div>
 
-          <Image
+          <div className="xsm:hidden"><Image
             src="/Images/iPhone12_Screen.png"
             alt="iPhone Screen"
-            className="h-[55vh] w-[30vw] shadow-sm object-contain ml-[4vw]"
+            className="h-[55vh] w-[30vw] shadow-sm object-contain ml-[4vw] "
             height={1000}
             width={1000}
-          />
+          /></div>
         </div>
       </div>
     </div>
--- src/components/Home/Supakling.tsx
+++ src/components/Home/Supakling.tsx
@@ -63,29 +63,29 @@ const Supakling = () => {
   };
 
   return (
-    <div className="pt-10">
+    <div className="p-10 overflow-hidden md:mt-10">
       {loading && (
         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
           <div className="loader"></div>
         </div>
       )}
-      <div className="gap-4 px-4 md:px-[10%] mb-[10vh]">
+      <div className="gap-4 px-4 md:px-[10%] xsm:mr-7 mb-[10vh] xsm:-mt-12">
         <div className="flex gap-6">
           {/* Left Section */}
-          <div className="flex sm:flex-col xsm:flex-col md:flex-col md:w-1/2 md:xsm-1/2 mb-4 md:mb-0">
+          <div className="flex xsm:flex-col sm:flex-col md:flex-col md:w-1/2 md:xsm-1/2 mb-4 md:mb-0 xsm:mt-14">
             <Image
               src="/Images/AllImages/Home_image_01.png"
               alt="Placeholder"
-              className="w-[15vw] h-auto rounded-lg md:w-[20vw]"
+              className="w-[15vw] h-auto rounded-lg md:w-[20vw] xsm:w-[30vw]"
               height={200}
               width={200}
             />
-            <div className="flex flex-row pt-3 justify-start items-center gap-y-2 gap-5">
-              <p className="text-lg md:text-[24px] font-dm-sans tracking-wide leading-relaxed ">126</p>
+            <div className="flex flex-row pt-3 xsm:pt-1 justify-start items-center gap-y-2 gap-5 xsm:gap-2">
+              <p className="text-lg md:text-[24px] xsm:text-[10px] font-dm-sans tracking-wide leading-relaxed ">126</p>
               <span className="text-[#AEADF1] font-bold text-lg md:text-[24px]">
                 +
               </span>
-              <p className="text-sm md:text-[16px] w-auto font-dm-sans tracking-wide leading-relaxed text-[#6B6A7E]">
+              <p className="text-sm md:text-[16px] w-auto font-dm-sans tracking-wide xsm:text-[5px] leading-relaxed text-[#6B6A7E]">
                 Professional Tools
               </p>
             </div>
@@ -93,15 +93,15 @@ const Supakling = () => {
 
           {/* Middle Section */}
           <div className="flex flex-col w-full md:w-2/2 pl-0 ">
-            <div className="flex flex-row justify-between md:flex-col md:items-start">
-              <h1 className="text-[#51DC98] uppercase font-bold text-md md:text-md">
+            <div className="flex flex-row justify-between md:flex-col md:items-start ">
+              <h1 className="text-[#51DC98] uppercase font-bold text-md md:text-md xsm:text-xs xsm:relative xsm:top-9">
                 {"/ Supaklin - Cleaning - Service".split(" ").join(" ")}
               </h1>
-              <div className="hidden md:block">
+              <div className=" md:block">
                 <Image
                   src="/Images/Home_Star_Image.png"
                   alt="Star Icon"
-                  className="w-12 md:w-[5vw] md:ml-[35vw] md:bottom-0 h-auto object-contain"
+                  className="w-12 md:w-[6vw] md:ml-[35vw] md:bottom-0 h-auto object-contain xsm:-ml-32 xsm:mt-16   xsm:absolute"
                   height={100}
                   width={100}
                 />
@@ -109,55 +109,57 @@ const Supakling = () => {
             </div>
 
             {/* Right Section */}
-            <div className="flex flex-col md:flex-row justify-between pt-10">
-              <div>
-                <h1 className="text-4xl md:text-[70px] font-bold tracking-[0.05em] leading-tight">
+            <div className="flex flex-col xsm:flex-row md:flex-row justify-between pt-10 md:pt-0">
+              <div >
+                <h1 className="text-4xl md:text-[40px] xsm:text-[20px] font-bold tracking-[0.05em] leading-tight">
                   Sparkling
                 </h1>
-                <h1 className="text-4xl md:text-[70px] font-bold tracking-[0.05em] leading-tight">
+                <h1 className="text-4xl md:text-[40px] xsm:text-[20px] font-bold tracking-[0.05em] leading-tight">
                   Clean Spaces
                 </h1>
               </div>
-              <div className="w-full md:w-[15vw]  mt-4 md:mt-0">
-                <p className="font-bold  font-lexend text-lg md:text-[24px] text-[rgba(36,35,42,1)]">
+              <div className="w-full md:w-[15vw] xsm:w-screen xsm:-ml-3 xsm:mr-10  mt-4 md:mt-0 xsm:mt-20">
+                <p className="font-bold  font-lexend text-lg md:text-[24px] xsm:text-[10px] text-[rgba(36,35,42,1)]">
                   Exceptional Service
                 </p>
-                <p className="text-sm md:text-[22px] mb-3 pt-5 md:w-[12vw] lg:w-[20vw] font-dm-sans tracking-wide leading-relaxed text-[#6B6A7E]" >
+                <p className="text-sm md:text-[10px] xsm:text-[10px] mb-3 pt-5 xsm:pt-1 md:w-full lg:w-[20vw] font-dm-sans tracking-wide xsm:tracking-tight leading-relaxed xsm:leading-tight text-[#6B6A7E]" >
                   Choose our exceptional services for quality, efficiency, and
                   attention to detail that meets every need
                 </p>
-
-                <button
-                  className="bg-[#0054A5] text-white rounded-full px-4 py-2 md:w-[10vw] hover:scale-105"
+              </div>
+              
+            </div>
+            
+              <button
+                  className="bg-[#0054A5] lg:w-[20%] lg:absolute lg:right-32 lg:bottom-32  text-white rounded-full px-4 py-2 md:w-full  xsm:w-24 xsm:px-2 xsm:text-sm hover:scale-105 relative xsm:-left-10 xsm:-top-12 "
                   onClick={handleGoContact}
                 >
                   Learn More
                 </button>
-              </div>
-            </div>
+           
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col justify-between h-auto md:h-screen pt-10 md:pt-[40vh]">
+      <div className="flex flex-col justify-between h-auto xsm:-mt-14 md:h-screen pt-10 md:pt-[40vh]">
         <div
-          className="bg-[#0054A5] w-full h-auto md:h-[50vh] flex justify-center items-center 2xl:items-start 2xl:justify-start  relative "
+          className="bg-[#0054A5] w-full h-auto md:w-full  xsm:h-[20vh] md:h-[30vh] flex justify-center items-center 2xl:items-start 2xl:justify-start  relative "
           onMouseMove={handleMouseMove}
         >
           {/* Video Embed */}
-          <div className="transform -translate-y-1/3 relative 2xl:px-40">
+          <div className="transform -translate-y-1/3 relative 2xl:px-40 xsm:p-7">
             <video
               autoPlay
               loop
               ref={videoRef}
-              className="w-full md:w-[60vw] h-[30vh] md:h-[60vh] 2xl:w-[100vw] object-cover rounded-xl shadow-sm"
+              className="w-full md:w-[60vw] h-[30vh]  md:h-[30vh] 2xl:w-[100vw] xsm:h-[20vh] xsm:w-[70vw] object-cover rounded-xl shadow-sm"
               src="/Images/solar.mp4"
             ></video>
 
             {/* Play/Pause Button */}
             {showButton && (
               <button
-                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#5653E1] text-xl md:text-3xl bg-[#FFFFFF] rounded-full p-4 md:p-8 z-10 cursor-pointer shadow-lg"
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#5653E1] text-xl md:text-3xl bg-[#FFFFFF] rounded-full p-4  md:p-8 z-10 cursor-pointer shadow-lg"
                 onClick={togglePlayPause}
               >
                 {isPlaying ? <FaPause size={20} /> : <FaPlay size={20} />}
